98cba9b20c27ef6c6a02e4f5725915486538a31f
Darius Kasperavicius <darius@foxrate.de>
2015-01-23 07:14:41 +0200

    Introduce TwitterTask bundle

--- src/Dardarlt/Bundle/TwitterTaskBundle/Controller/DefaultController.php
+++ src/Dardarlt/Bundle/TwitterTaskBundle/Controller/DefaultController.php
@@ -3,11 +3,13 @@
 namespace Dardarlt\Bundle\TwitterTaskBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('DardarltTwitterTaskBundle:Default:index.html.twig', array('name' => $name));
+        $data = $this->get('tt.twitter_provider')->getStatuses();
+        return new JsonResponse($data);
     }
 }
