4a5c788d5ec4895c159b7173134139c9e6d39601
Darius <dardarlt@gmail.com>
2015-01-23 12:39:29 +0200

    Update  controller test , check if service is not found with incorect parameters

--- src/Dardarlt/Bundle/TwitterTaskBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Dardarlt/Bundle/TwitterTaskBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,15 +3,23 @@
 namespace Dardarlt\Bundle\TwitterTaskBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testServiceIsWorking()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/task2/tweets/justin_bieber');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Tweets")')->count() > 0);
+    }
+
+    public function testServiceNotFound()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/task2/tweets/random_user_not');
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
     }
 }
